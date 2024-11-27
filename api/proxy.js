8199581385d37f5da9d3095dcd28bf6a872139a0
Roman Tyan <Roman.Tyan@monitel.ru>
2024-11-27 16:33:28 +0300

    chrom ver

--- api/proxy.js
+++ api/proxy.js
@@ -13,7 +13,7 @@ app.get("/*", async (req, res) => {
 
     // Устанавливаем User-Agent и другие заголовки
     await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.85"
     );
 
     await page.goto(url, { waitUntil: "domcontentloaded" });
