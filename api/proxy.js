cc48a8cef79bd0be58c37bdc84d9b19a0f731964
Roman Tyan <Roman.Tyan@monitel.ru>
2024-11-27 16:36:49 +0300

    aws chrome

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,5 @@
-const puppeteer = require("puppeteer");
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 const express = require("express");
 const app = express();
@@ -7,19 +8,25 @@ const PORT = process.env.PORT || 3000;
 app.get("/*", async (req, res) => {
   const url = `https://x-minus.pro/${req.params[0] || ""}`;
   try {
-    console.log(`Opening URL in Puppeteer: ${url}`);
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    console.log(`Proxying request to: ${url}`);
 
-    // Устанавливаем User-Agent и другие заголовки
+    // Настройка Puppeteer для использования Chrome-AWS-Lambda
+    const browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    });
+
+    const page = await browser.newPage();
     await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.85"
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     );
 
+    // Переход на целевой URL
     await page.goto(url, { waitUntil: "domcontentloaded" });
-
-    // Извлекаем содержимое страницы
     const content = await page.content();
+
+    // Возвращаем содержимое страницы
     res.send(content);
 
     await browser.close();
